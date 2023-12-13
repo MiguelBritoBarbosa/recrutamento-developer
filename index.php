205bc0d2774f1225ebace07ae8cfc7c824adf37a
breno30 <brenodonascimentosilvaa@gmail.com>
2023-12-13 17:26:10 -0300

    create query to filter posts

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
-<?php get_header();
+<?php
+get_header();
 $botao = 'Continue lendo';
+
+$args = array(
+    'category_name' => 'blog',
+    'post_type' => 'post',
+    'meta_query' => array(
+        array(
+            'key' => 'status',
+            'value' => 'free',
+            'compare' => '=',
+        ),
+    ),
+    'date_query' => array(
+        array(
+            'after' => '2019-09-10',
+            'before' => '2019-09-20',
+            'inclusive' => true,
+        ),
+    ),
+);
+
+$query = new WP_Query($args);
 ?>
 <div class="container-page">
 	<div class="header-page" style="background: url('<?php bloginfo('template_url'); ?>/src/images/bg-blog.jpg') no-repeat center center;background-size: cover;">
@@ -13,99 +35,102 @@ $botao = 'Continue lendo';
 			<div class="top-itens">
 				<div class="center-small">
 					<div class="col">
-						<div class="item">
-							<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" title="Blanditiis velit esse cumque minima consequatur">
-								<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-post-big.jpg">
-							</a>
-							<div class="wrapper">
-								<div class="category">
-									Blog
-								</div>
-								<div class="text">
-									<h2 class="title">
-										<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" title="Blanditiis velit esse cumque minima consequatur">Blanditiis velit esse cumque minima consequatur</a>
-									</h2>
-									<div class="text-mobile">
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam delectus sed molestias deserunt. Assumenda illum eum placeat cum repellat quisquam cumque doloribus distinctio, dol...</p>
+						<?php
+							if ($query->have_posts()) :
+								$query->the_post();
+							?>
+							<div class="item">
+								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+									<img src="./wp-content/themes/recrutamento-developer/src/images/no-thumb-post-big.jpg">
+								</a>
+								<div class="wrapper">
+									<div class="category">
+										<?php echo get_the_category_list(', '); ?>
 									</div>
-									<a href="./blanditiis-velit-esse-cumque-minima-consequatur/" class="link-more" title="Continue lendo Blanditiis velit esse cumque minima consequatur">Continue lendo</a>
+									<div class="text">
+										<h2 class="title">
+											<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+										</h2>
+										<div class="text-mobile">
+											<p><?php echo wp_trim_words(get_the_content(), 20, '...'); ?></p>
+										</div>
+										<a href="<?php the_permalink(); ?>" class="link-more" title="Continue lendo <?php the_title(); ?>">Continue lendo</a>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
+						<?php
+							endif;
+						?>
+				</div>
+
 					<div class="col">
-						<div class="item item-small">
-							<a href="./atque-enim-consectetur-quod/" title="Atque enim consectetur quod">
-								<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-post-small.jpg">
-							</a>
-							<div class="wrapper">
-								<div class="category">
-									Blog
+						<?php
+							rewind_posts();
+							if ($query->have_posts()) :
+								$query->next_post();
+								$counter = 0;
+								while ($query->have_posts() && $counter < 2) : $query->the_post();
+						?>
+								<div class="item item-small">
+									<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+										<img src="./wp-content/themes/recrutamento-developer/src/images/no-thumb-post-small.jpg">
+									</a>
+									<div class="wrapper">
+										<div class="category">
+											<?php echo get_the_category_list(', '); ?>
+										</div>
+										<div class="text">
+											<h2 class="title">
+												<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+											</h2>
+											<div class="text-mobile">
+												<p><?php echo wp_trim_words(get_the_content(), 20, '...'); ?></p>
+											</div>
+											<a href="<?php the_permalink(); ?>" class="link-more" title="Continue lendo <?php the_title(); ?>">Continue lendo</a>
+										</div>
+									</div>
 								</div>
-								<div class="text">
-									<h2 class="title">
-										<a href="./atque-enim-consectetur-quod/" title="Atque enim consectetur quod">Atque enim consectetur quod</a>
-									</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea non dolorem, a consequatur debitis voluptas accusantium veritatis. Itaque officia veniam maiores commodi, atque enim...</p>
-									<a href="./atque-enim-consectetur-quod/" class="link-more" title="Continue lendo Atque enim consectetur quod">Continue lendo</a>
-								</div>
-							</div>
-						</div>
-						<div class="item item-small">
-							<a href="./lorem-ipsum-dolor-sit-amet/" title="Lorem ipsum dolor sit amet">
-								<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-post-small.jpg">
-							</a>
-							<div class="wrapper">
-								<div class="category">
-									Blog
-								</div>
-								<div class="text">
-									<h2 class="title">
-										<a href="./lorem-ipsum-dolor-sit-amet/" title="Lorem ipsum dolor sit amet">Lorem ipsum dolor sit amet</a>
-									</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea non dolorem, a consequatur debitis voluptas accusantium veritatis. Itaque officia veniam maiores commodi, atque enim...</p>
-									<a href="./lorem-ipsum-dolor-sit-amet/" class="link-more" title="Continue lendo Lorem ipsum dolor sit amet">Continue lendo</a>
-								</div>
-							</div>
-						</div>
+						<?php
+								$counter++;
+							endwhile;
+						endif;
+						?>
 					</div>
 				</div>
 			</div>
+
 			<div class="itens-post">
-				<div class="item">
-					<div class="wrapper">
-						<div class="image">
-							<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-big.jpg">
-						</div>
-						<div class="text">
-							<h2>
-								<a href="#" title="novo teste de conteúdo">novo teste de conteúdo</a>
-							</h2>
-							<span class="icons-post icon-date">
-								<i class="icon-calendar"></i>18.07.2019
-							</span>
-							<a href="#" class="icons-post icon-coment-count">
-								<i class="icon-comment-alt"></i>0
-							</a>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci accusantium repudiandae aperiam dolorem error consequatur harum nam distinctio, voluptates mollitia excepturi fac</p>
-							<a class="link-more" href="#" title="novo teste de conteúdo">Continue lendo</a>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="wrapper">
-						<div class="image">
-							<img src="./wp-content/themes/somadev-theme-master/src/images/no-thumb-big.jpg">
-						</div>
-						<div class="text">
-							<h2><a href="#" title="Dolor impedit soluta perspiciatis fugiat">Dolor impedit soluta perspiciatis fugiat</a></h2>
-							<span class="icons-post icon-date"><i class="icon-calendar"></i>17.07.2019</span>
-							<a href="#" class="icons-post icon-coment-count"><i class="icon-comment-alt"></i>0</a>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam delectus sed molestias deserunt. Assumenda illum eum placeat cum repellat quisquam cumque doloribus distinctio, dolor </p>
-							<a class="link-more" href="#" title="Dolor impedit soluta perspiciatis fugiat">Continue lendo</a>
-						</div>
-					</div>
-				</div>
+				<?php
+						if ($query->have_posts()) :
+							while ($query->have_posts()) : $query->the_post();
+						?>
+							<div class="item">
+								<div class="wrapper">
+									<div class="image">
+										<img src="./wp-content/themes/recrutamento-developer/src/images/no-thumb-post-small.jpg">
+									</div>
+									<div class="text">
+										<h2>
+											<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+										</h2>
+										<span class="icons-post icon-date">
+											<i class="icon-calendar"></i><?php the_date(); ?>
+										</span>
+										<a href="<?php the_permalink(); ?>" class="icons-post icon-coment-count">
+											<i class="icon-comment-alt"></i><?php get_comments_number(); ?>
+										</a>
+										<p><?php the_excerpt(); ?></p>
+										<a class="link-more" href="<?php the_permalink(); ?>" title="novo teste de conteúdo">Continue lendo</a>
+									</div>
+								</div>
+							</div>
+				<?php
+					endwhile;
+						wp_reset_postdata();
+							else :
+								echo 'Nenhum post encontrado';
+							endif;
+				?>
 			</div>
 		</div>
 	</div>
